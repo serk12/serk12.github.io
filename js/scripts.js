6439f2d509ac1e07b246bfb6b7f7d0d7c131f6fb
serk12 <marc.prat.maso@est.fib.upc.edu>
2020-04-19 21:17:21 +0200

    First customization

--- js/scripts.js
+++ js/scripts.js
@@ -9,276 +9,218 @@
     Description: This file contains all the scripts associated with the single-page
     portfolio website.
 */
-AOS.init({duration: 1000, mirror: true, ease: 'ease-in-out-quart'});
+AOS.init({duration: 1000, mirror: true, ease: "ease-in-out-quart"});
 $(document).ready(function() {
-  var app = document.getElementById('typewriter');
-  var typewriter = new Typewriter(app, {
-      loop: true,
-      strings: ['Security Researcher', 'Software Developer', 'Bug Hunter', 'Avid Reader'],
-      autoStart: true,
-  });
-setTimeout( function(){
-    $('#mobile-menu-open').addClass('fixed');
-},1000);
-    $('#mobile-menu-open').css('position','fixed');
+	var app = document.getElementById("typewriter");
+	var typewriter = new Typewriter(app, {
+		loop: true,
+		strings: [
+			"Graphics Programmer",
+			"Software Developer",
+			"Simulation Researcher",
+			"Data Science"
+		],
+		autoStart: true
+	});
+	setTimeout(function() {
+		$("#mobile-menu-open").addClass("fixed");
+	}, 1000);
+	$("#mobile-menu-open").css("position", "fixed");
 });
 
 (function($) {
-    // Remove no-js class
-    $('html').removeClass('no-js');
+	// Remove no-js class
+	$("html").removeClass("no-js");
 
-    // Animate to section when nav is clicked
-    $('header a').click(function(e) {
+	// Animate to section when nav is clicked
+	$("header a").click(function(e) {
+		// Treat as normal link if no-scroll class
+		if ($(this).hasClass("no-scroll")) return;
 
-        // Treat as normal link if no-scroll class
-        if ($(this).hasClass('no-scroll')) return;
+		e.preventDefault();
+		var heading = $(this).attr("href");
+		var scrollDistance = $(heading).offset().top;
 
-        e.preventDefault();
-        var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
+		$("html, body").animate(
+			{
+				scrollTop: scrollDistance + "px"
+			},
+			Math.abs(window.pageYOffset - $(heading).offset().top) / 1
+		);
 
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+		// Hide the menu once clicked if mobile
+		if ($("header").hasClass("active")) {
+			$("header, body").removeClass("active");
+		}
+	});
 
-        // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
-            $('header, body').removeClass('active');
-        }
-    });
+	// Scroll to top
+	$("#to-top").click(function() {
+		$("html, body").animate(
+			{
+				scrollTop: 0
+			},
+			500
+		);
+	});
 
-    // Scroll to top
-    $('#to-top').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
+	// Scroll to first element
+	$("#lead-down span").click(function() {
+		var scrollDistance = $("#lead")
+			.next()
+			.offset().top;
+		$("html, body").animate(
+			{
+				scrollTop: "scrollDistance" + "px"
+			},
+			500
+		);
+	});
 
-    // Scroll to first element
-    $('#lead-down span').click(function() {
-        var scrollDistance = $('#lead').next().offset().top;
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, 500);
-    });
+	// Open mobile menu
+	$("#mobile-menu-open").click(function() {
+		$("header, body").addClass("active");
+	});
 
+	// Close mobile menu
+	$("#mobile-menu-close").click(function() {
+		$("header, body").removeClass("active");
+	});
 
-    // Open mobile menu
-    $('#mobile-menu-open').click(function() {
-        $('header, body').addClass('active');
-    });
+	// Load additional projects
+	$("#view-more-projects").click(function(e) {
+		e.preventDefault();
+		$(this).fadeOut(300, function() {
+			$("#more-projects").fadeIn(300);
+		});
+	});
 
-    // Close mobile menu
-    $('#mobile-menu-close').click(function() {
-        $('header, body').removeClass('active');
-    });
+	$(document).ready(function() {
+		var baseData = {
+			datasets: [
+				{
+					data: [100, 50, 200, 150, 40],
+					backgroundColor: ["#60BD68", "#5DA5DA", "#FAA43A", "#F15854", "#a6d746"]
+				}
+			],
+			labels: [
+				"Web Development",
+				"Data Science",
+				"Programming",
+				"Graphics",
+				"Others"
+			],
+			hoverBorderWidth: 8
+		};
+		var baseoptions = {
+			rotation: 0 * Math.PI,
+			circumference: 2 * Math.PI,
+			elements: {
+				arc: {
+					borderWidth: 3
+				}
+			},
+			tooltips: {
+				displayColors: false,
+				enabled: true,
+				mode: "single",
+				callbacks: {
+					label: function(tooltipItems, data) {
+						return data.labels[tooltipItems.index];
+					},
+					afterLabel: function(tooltipItems, data) {
+						return "Click Me!";
+					}
+				}
+			},
+			legend: {
+				position: "right",
+				onClick: function(event, legendItem) {},
+				labels: {
+					fontSize: 20,
+					fontColor: "#ffffff"
+				}
+			},
+			segmentShowStroke: false
+		};
+		var canvas = document.getElementById("mySkills");
+		var ctx = canvas.getContext("2d");
+		var myNewChart = new Chart(ctx, {
+			type: "doughnut",
+			data: baseData,
+			options: baseoptions
+		});
+		canvas.onclick = function(evt) {
+			var activePoints = myNewChart.getElementsAtEvent(evt);
+			if (activePoints[0]) {
+				var chartData = activePoints[0]["_chart"].config.data;
+				var idx = activePoints[0]["_index"];
 
-    // Load additional projects
-    $('#view-more-projects').click(function(e){
-        e.preventDefault();
-        $(this).fadeOut(300, function() {
-            $('#more-projects').fadeIn(300);
-        });
-    });
-
-    $(document).ready(
-      function() {
-        var baseData = {
-          datasets: [{
-            data: [250, 200, 50, 200, 250, 50 ],
-            backgroundColor: [
-              "#60BD68",
-              "#5DA5DA",
-              "#FAA43A",
-              "#F17CB0",
-              "#B2912F",
-              "#F15854"
-            ]
-          }],
-          labels: [
-            "Web Development",
-            "DevOps",
-            "Machine Learning",
-            "Programming",
-            "Security",
-            "DBMS"
-          ],
-          hoverBorderWidth: 8
-        };
-        var baseoptions = {
-          rotation: 0 * Math.PI,
-          circumference: 2 * Math.PI,
-          elements: {
-            arc: {
-              borderWidth: 0
-            }
-          },
-          tooltips: {
-              displayColors: false,
-              enabled: true,
-              mode: 'single',
-              callbacks: {
-                  label: function(tooltipItems, data) {
-                      return data.labels[tooltipItems.index];
-                  },
-                  afterLabel: function(tooltipItems, data) {
-                      return "Click Me!";
-                  }
-              }
-          },
-          legend: {position: 'left', onClick: function(event, legendItem) {}},
-          segmentShowStroke: false
-          }
-        var canvas = document.getElementById("mySkills");
-        var ctx = canvas.getContext("2d");
-        var myNewChart = new Chart(ctx, {
-          type: 'doughnut',
-          data: baseData,
-          options: baseoptions
-        });
-        canvas.onclick = function(evt) {
-          var activePoints = myNewChart.getElementsAtEvent(evt);
-          if (activePoints[0]) {
-            var chartData = activePoints[0]['_chart'].config.data;
-            var idx = activePoints[0]['_index'];
-
-            var label = chartData.labels[idx];
-            var data = baseData;
-            var options = baseoptions
-            options.circumference = 2 * Math.PI;
-            if (label == baseData.labels[0]) {
-              var data = {
-                datasets: [{
-                  data: [200, 100, 50, 100, 50],
-                  backgroundColor: [
-                    "#208326",
-                    "#8DD692",
-                    "#BDEAC0",
-                    "#3A9F41",
-                    "#60BD66"
-                  ]
-                }],
-                labels: [
-                  "Django",
-                  "Tornado/Asyncio",
-                  "Vue.js",
-                  "Bootstrap",
-                  "Webpack"
-                ]
-              };
-            }
-
-          else if (label == baseData.labels[1]) {
-            var data = {
-              datasets: [{
-                data: [75, 75, 200, 50, 100],
-                backgroundColor: [
-                  "#85C0E9",
-                  "#3C8EC8",
-                  "#1D74B2",
-                  "#B9DDF6",
-                  "#5DA6DA",
-
-                ]
-              }],
-              labels: [
-                "Jenkins",
-                "Kubernetes",
-                "Docker",
-                "ELK",
-                "AWS"
-              ]
-            };
-          }
-          else if (label == baseData.labels[2]) {
-            var data = {
-              datasets: [{
-                data: [350, 200],
-                backgroundColor: [
-                  "#E5840D",
-                  "#FFB75F",
-                ]
-              }],
-              labels: [
-                "TensorFlow",
-                "SciKit-Learn"
-              ]
-            };
-          }
-          else if (label == baseData.labels[3]) {
-            var data = {
-              datasets: [{
-                data: [200, 100, 125, 75],
-                backgroundColor: [
-                  "#DB387F",
-                  "#F17CAF",
-                  "#E75897",
-                  "#F8A7CA",
-                ]
-              }],
-              labels: [
-                "Python",
-                "Bash",
-                "C/C++",
-                "Javascript"
-              ]
-            };
-          }
-          else if (label == baseData.labels[4]) {
-            var data = {
-              datasets: [{
-                data: [125, 50, 75, 150, 100],
-                backgroundColor: [
-                  "#907013",
-                  "#FBDF8D",
-                  "#B2912F",
-                  "#644B00",
-                  "#D2B253",
-
-                ]
-              }],
-              labels: [
-                "OWASP ZAP",
-                "IDA pro",
-                "Wireshark",
-                "GDB",
-                "Sqlmap"
-              ]
-            };
-          }
-          else if (label == baseData.labels[5]) {
-            var data = {
-              datasets: [{
-                data: [175, 125, 50, 25, 125],
-                backgroundColor: [
-                  "#AF1915",
-                  "#DB322E",
-                  "#FF7F7C",
-                  "#FFA9A7",
-                  "#F15854",
-                ]
-              }],
-              labels: [
-                "Postgres",
-                "MongoDB",
-                "Memcached",
-                "Redis",
-                "Sql",
-              ]
-            };
-          }
-          else{
-            options.rotation = 0;
-            options.circumference = 2 * Math.PI;
-          }
-          myNewChart.destroy();
-          myNewChart = new Chart(ctx, {
-            type: 'doughnut',
-            data: data,
-            options: options
-          });
-          }
-        };
-      }
-    );
+				var label = chartData.labels[idx];
+				var data = baseData;
+				var options = baseoptions;
+				options.circumference = 2 * Math.PI;
+				if (label == baseData.labels[0]) {
+					var data = {
+						datasets: [
+							{
+								data: [150, 150, 100, 100],
+								backgroundColor: ["#208326", "#8DD692", "#BDEAC0", "#3A9F41", "#60BD66"]
+							}
+						],
+						labels: ["Django", "Symfony-PHP", "Perl"]
+					};
+				} else if (label == baseData.labels[1]) {
+					var data = {
+						datasets: [
+							{
+								data: [35, 20, 20],
+								backgroundColor: ["#85C0E9", "#3C8EC8", "#1D74B2", "#B9DDF6", "#5DA6DA"]
+							}
+						],
+						labels: ["R", "Mathlab", "Pytorch"]
+					};
+				} else if (label == baseData.labels[2]) {
+					var data = {
+						datasets: [
+							{
+								data: [50, 30, 10, 10],
+								backgroundColor: ["#E5840D", "#FFB75F", "#fbcd94", "#fad9b0"]
+							}
+						],
+						labels: ["C/C++", "Python", "Haskell", "Prolog"]
+					};
+				} else if (label == baseData.labels[3]) {
+					var data = {
+						datasets: [
+							{
+								data: [30, 50, 40],
+								backgroundColor: ["#AF1915", "#DB322E", "#FF7F7C", "#FFA9A7", "#F15854"]
+							}
+						],
+						labels: ["Qt", "OpenGL", "SFML"]
+					};
+				} else if (label == baseData.labels[4]) {
+					var data = {
+						datasets: [
+							{
+								data: [30, 50, 40, 20],
+								backgroundColor: ["#91d016", "#a6d746", "#b3d96a", "#b6cd87"]
+							}
+						],
+						labels: ["Docker", "Git", "CI-CD", "Atom"]
+					};
+				} else {
+					options.rotation = 0;
+					options.circumference = 2 * Math.PI;
+				}
+				myNewChart.destroy();
+				myNewChart = new Chart(ctx, {
+					type: "doughnut",
+					data: data,
+					options: options
+				});
+			}
+		};
+	});
 })(jQuery);
